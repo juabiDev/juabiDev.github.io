30a5690b63af5307459c259084067011332d168d
juabiDev <Mederos40@outlook.com>
2023-10-15 12:39:48 -0300

    feature: deleted unnecesary code

--- app.js
+++ app.js
@@ -1,42 +1,9 @@
-window.addEventListener("load",inicio);
+document.addEventListener("DOMContentLoaded",inicio);
 
 function inicio() {
-/*     hover(); */
     hoverArrowProject();
     scrollAnimation();
 }
-
-/* 
- // hecho con CSS
-
-function hover() {
-    let botones = document.getElementsByClassName("boton");
-    
-    for (let i = 0; i < botones.length; i++) {
-        botones[i].addEventListener("mouseover",blanco);
-        botones[i].addEventListener("mouseout",negro);  
-    } 
-
-    function blanco(event) {
-        
-        if(event.target.nodeName == "svg") {
-            event.target.setAttribute("stroke","#fff");
-            event.target.setAttribute("fill","#fff");
-        } else if(event.target.nodeName == "path") {
-            event.target.parentElement.setAttribute("stroke","#fff");
-            event.target.parentElement.setAttribute("fill","#fff");
-        }
-
-    }
-
-    function negro(event) {
-
-        if(event.target.nodeName == "svg") {
-            event.target.setAttribute("stroke","currentColor");
-            event.target.setAttribute("fill","currentColor");    
-        } 
-    }
-} */
 
 function hoverArrowProject() {
     let flechas = document.getElementsByClassName("project__link");
@@ -72,29 +39,9 @@ function scrollAnimation() {
         let windowHeight = window.innerHeight / 2;
         let topSkills = skillsSection.getBoundingClientRect().top;
 
-/*         
-        Animando Ambas Cosas
-
-        if(topProjects < windowHeight) {
-            projectListSection.classList.add("showScale");
-            setTimeout(() => {
-                projectListSection.classList.add("pausarAnimacion");
-                projectListSection.classList.add("showItem"); 
-            }, 1700); 
-        } else {
-            projectListSection.classList.remove("showList");
-        } 
-        
-        */
-
-        //Animando solo el blur
-
         if(topProjects < windowHeight) {
             projectListSection.classList.add("showItem");
-
-            setTimeout(() => {
-                projectListSection.classList.add("pausarAnimacion"); 
-            }, 1700); 
+            projectListSection.classList.add("pausarAnimacion"); 
         } else {
             projectListSection.classList.remove("showItem");
         }
